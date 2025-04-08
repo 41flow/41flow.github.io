eb4fa111cc26477b22524b67b1e0048dd839d26d
randomamazingguy <199291680+randomamazingguy@users.noreply.github.com>
2025-04-08 04:33:57 +0100

    as

--- script.js
+++
@@ -1,7 +0,0 @@
-const toggleButton = document.getElementById('themeToggle');
-const body = document.body;
-
-toggleButton.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
-    toggleButton.textContent = body.classList.contains('dark-mode') ? '☀️' : '🌙';
-});
